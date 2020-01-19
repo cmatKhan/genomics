cbd07873c756254ed1a94c99d164a674623e0cee
chase mateusiak <chase.mateusiak@gmail.com>
2020-01-19 17:36:26 -0600

    completed part 2, 3 and 5

--- assignment1/nuc_count.py
+++ assignment1/nuc_count.py
@@ -13,12 +13,12 @@ import sys
 
 # sys.arg is a list containing 2 elements: the script name and 1 command line argument
 # Check that all the command line argument was given. If not, print the documentation and exit.
-if (len(sys.argv) != 2):
-    sys.exit(__doc__) 
+#if (len(sys.argv) != 2):
+#    sys.exit(__doc__)
 
 # Save the input arguments as variables
-fasta = sys.argv[1]
-
+#fasta = sys.argv[1]
+fasta = '/home/chase/Downloads/chr20.fna'
 # Initialize a nucleotide string
 nucleotides = ""
 
@@ -50,13 +50,51 @@ print ("G: ", num_g)
 print ("T: ", num_t)
 print ("N: ", num_n)
 
-
 ## Part 3
-### TODO Print out the frequencies for each nucleotide in alphabetical order
+
+# create dictionary
+nuc_dict = {"A": num_a, "T": num_t, "G": num_g, "C": num_c, "N": num_n}
+
+# total nucleotides
+chr20_total = 0
+chr20_total = sum(nuc_dict.values())
+
+# print frequencies -- N is included in the total calculation
+for nuc in ["A", "T", "G", "C"]:
+    nuc_freq = nuc_dict[nuc]/chr20_total
+    print("\n The frequency of {} is {}".format(nuc, nuc_freq))
+
+## Part 4
+
 
 
 ## Part 5
-### TODO Use overlapping windows to count the dinucleotides. See the assignment for more information on overlapping windows.
-### TODO Print the frequencies for each of dinucleotides in alphabetical order
+
+# create dictionary to store key, value pairs {dinucleotide: total}
+dinuc_dict={}
+
+for i in range(len(nucleotides)-1):
+    # concat the ith and i+1 element of string
+    dinuc = nucleotides[i] + nucleotides[i+1]
+    # pass if N
+    if 'N' in dinuc:
+        pass
+    else:
+        # test if given dinuc is in dictrion
+        if (dinuc_dict.setdefault(dinuc, False)):
+            # if so, add one to the value associated with the given dinuc
+            dinuc_dict[dinuc] = dinuc_dict[dinuc] + 1
+        else:
+            # if not, create a new key (the dinuc) in the dictionary and associate it with a value of 1
+            dinuc_dict[dinuc] = 1
+
+# take sum of dinucleotides
+total_dinuc = sum(dinuc_dict.values())
+
+# I did look up how to sort the keys of a dictionary. Answer found here: https://stackoverflow.com/questions/16600174/return-output-of-dictionary-to-alphabetical-order
+for key, value in sorted(dinuc_dict.items()):
+    dinuc_freq = value/total_dinuc
+    print("The frequency of dinucleotide sequence {} in chromosome 20 is {}". format(key, dinuc_freq))
+
 
 
