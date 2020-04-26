9572b91915e5c8e03d96059a42ed49678a5726d8
cmatkhan <chase.mateusiak@gmail.com>
2020-04-26 12:20:41 -0500

    complete assign13

--- assignment13/Polk.py
+++ assignment13/Polk.py
@@ -1,65 +1,173 @@
 #!/usr/bin/env python3
-"""Script that takes in amino acid sequence as input and outputs
-all possible DNA sequences that could encode this AA.
-Usage: python3 Polk.py <peptide sequence>"""
+"""
+    Script that takes in amino acid sequence as input and outputs
+    all possible DNA sequences that could encode this AA.
+    Usage: python3 Polk.py -p <peptide sequence>
+"""
 
-#Make sure # of arguments is correct
 import sys
-if len(sys.argv)!=2:
-        sys.exit( __doc__)
+import os
+import argparse
+import logging
 
-#Standard table of codons - a dictionary of single-letter
-#amino acid code to a list of codon choices
+def main(argv):
+    """
+    main method
+    :param argv: cmd line args
+    """
+    # create global variable to store aa_to_codons dict (available to all methods)
+    global aa_to_codons
+    aa_to_codons = AminoAcidToCodonDict()
 
-aa_to_codons = {}
-aa_to_codons[ "A" ] = ["GCA", "GCC", "GCG", "GCT" ]
-aa_to_codons[ "C" ] = ["TGC", "TGT" ]
-aa_to_codons[ "D" ] = ["GAC", "GAT" ]
-aa_to_codons[ "E" ] = ["GAA", "GAG" ]
-aa_to_codons[ "F" ] = ["TTC", "TTT" ]
-aa_to_codons[ "G" ] = ["GGA", "GGC", "GGG", "GGT" ]
-aa_to_codons[ "H" ] = ["CAC", "CAT" ]
-aa_to_codons[ "I" ] = ["ATA", "ATC", "ATT" ]
-aa_to_codons[ "K" ] = ["AAA", "AAG" ]
-aa_to_codons[ "L" ] = ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG" ]
-aa_to_codons[ "M" ] = ["ATG" ]
-aa_to_codons[ "N" ] = ["AAC", "AAT" ]
-aa_to_codons[ "P" ] = ["CCA", "CCC", "CCG", "CCT" ]
-aa_to_codons[ "Q" ] = ["CAA", "CAG" ]
-aa_to_codons[ "R" ] = ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT" ]
-aa_to_codons[ "S" ] = ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT" ]
-aa_to_codons[ "T" ] = ["ACA", "ACC", "ACG", "ACT" ]
-aa_to_codons[ "V" ] = ["GTA", "GTC", "GTG", "GTT" ]
-aa_to_codons[ "W" ] = ["TGG" ]
-aa_to_codons[ "Y" ] = ["TAC", "TAT" ]
-aa_to_codons[ "*" ] = ["TAA", "TAG", "TGA" ]
+    # parse cmd line input
+    args = parseArgs(argv)
+    peptide_sequences = args.peptide_sequence
+    melting_temps = args.melting_temp
 
-def check_combinations(dna_string, aa_string):
-    ####Write a Doc string for this####
-    # if this code is confusing to you, uncomment the print statement
-    #print("Input DNA is:",dna_string,"Remaining AAs are:", aa_string, sep='\t')
+    # output file
+    output_file = 'polk_script_output.txt'
 
-    ####What's going on here####
+    # prompt user to rename or overwrite previous output, if exists
+    if os.path.exists(output_file):
+        response = input('do you wish to overwrite %s? If not, exist and rename. Otherwise, enter \'y\': ' %output_file)
+        if response == 'y':
+            cmd = 'rm polk_script_output.txt'
+            exit_status = os.system(cmd)
+            if exit_status == '1':
+                raise Exception('Could not remove polk_script_output.txt')
+
+    for sequence in peptide_sequences:
+        sequence = sequence.upper()
+        for melting_temp in melting_temps:
+            with open(output_file, 'a') as file:
+                melting_temp = float(melting_temp)
+                file.write("Amino Acid Sequence: %s\nMelting Temperature: %.2f\nDNA Sequences:\n" %(sequence, melting_temp))
+            checkCombinations("", sequence, melting_temp, output_file)
+            file.close()
+            with open(output_file, 'a') as file:
+                file.write('\n')
+            file.close()
+
+    # print output to stdout
+    cmd = 'cat ./%s' %output_file
+    exit_status = os.system(cmd)
+    if exit_status == '1':
+        raise Exception('Could print polk_script_output.txt. It may be that it is not being printed to $PWD')
+
+def parseArgs(argv):
+    parser = argparse.ArgumentParser(
+        description="")
+    parser.add_argument("-p", "--peptide_sequence", required=True, nargs='+',
+                        help='[REQUIRED] The peptide sequence of interest')
+    parser.add_argument("-m", "--melting_temp", required=True, nargs='+',
+                        help='[REQUIRED] The required melting temperature')
+    args = parser.parse_args(argv[1:])
+    return args
+
+def AminoAcidToCodonDict():
+    """
+    create amino acide to codon dictionary
+    :return: dictionary of structure { <amino acid symbol>: [list of codons] }
+    """
+    aa_to_codons = {}
+    aa_to_codons[ "A" ] = ["GCA", "GCC", "GCG", "GCT" ]
+    aa_to_codons[ "C" ] = ["TGC", "TGT" ]
+    aa_to_codons[ "D" ] = ["GAC", "GAT" ]
+    aa_to_codons[ "E" ] = ["GAA", "GAG" ]
+    aa_to_codons[ "F" ] = ["TTC", "TTT" ]
+    aa_to_codons[ "G" ] = ["GGA", "GGC", "GGG", "GGT" ]
+    aa_to_codons[ "H" ] = ["CAC", "CAT" ]
+    aa_to_codons[ "I" ] = ["ATA", "ATC", "ATT" ]
+    aa_to_codons[ "K" ] = ["AAA", "AAG" ]
+    aa_to_codons[ "L" ] = ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG" ]
+    aa_to_codons[ "M" ] = ["ATG" ]
+    aa_to_codons[ "N" ] = ["AAC", "AAT" ]
+    aa_to_codons[ "P" ] = ["CCA", "CCC", "CCG", "CCT" ]
+    aa_to_codons[ "Q" ] = ["CAA", "CAG" ]
+    aa_to_codons[ "R" ] = ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT" ]
+    aa_to_codons[ "S" ] = ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT" ]
+    aa_to_codons[ "T" ] = ["ACA", "ACC", "ACG", "ACT" ]
+    aa_to_codons[ "V" ] = ["GTA", "GTC", "GTG", "GTT" ]
+    aa_to_codons[ "W" ] = ["TGG" ]
+    aa_to_codons[ "Y" ] = ["TAC", "TAT" ]
+    aa_to_codons[ "*" ] = ["TAA", "TAG", "TGA" ]
+
+    return aa_to_codons
+
+def calculateMeltingTemp(dna_string):
+    """
+
+    :param dna_string:
+    :return:
+    """
+    num_G = float(dna_string.count('G'))
+    num_C = float(dna_string.count('C'))
+    return 64.9 + (41.0 * ((num_G + num_C -16.4) / float(len(dna_string))) )
+
+def checkCombinations(dna_string, aa_string, melting_temp_threshold, output_file):
+    """
+    Translate aa_string to all possible DNA strings (given the a dictionary of AA: [list of synonymous codons]
+    :param dna_string:
+    :param aa_string:
+    :param restriction_sites:
+    :return: a list of all possible DNA strings for the given aa_string
+    """
+    # restriction site list
+    restriction_sites = ['CATATG', 'CTCGAG', 'TCGA', 'CTAG']
+    # base case
     if (len(aa_string)==0):
-        print(dna_string)
+        # discard DNA strings with restriction sites
+        restriction_site = False
+        for seq in restriction_sites:
+            if seq in dna_string:
+                restriction_site = True
+        if not restriction_site:
+            melting_temp = calculateMeltingTemp(dna_string)
+            # do not print DNA strings if the melting temp threshold is not satisfied
+            if melting_temp_threshold - .5 <= melting_temp <= melting_temp_threshold + .5:
+                with open(output_file, 'a') as file:
+                    file.write('%s\t%.2f\t' %(dna_string, melting_temp))
 
-    ####What's going on here####
-    else:
+    # if the base case has not been reached...
+    elif len(aa_string) != 0:
 
-        ####What's going on here####
-        current_AA = aa_string[0];
+        # look at the first amino acid in aa_string
+        current_AA = aa_string[0]
 
-        ####What's going on here####
+        # iterate over codons in the list corresponding to the dictionary entry in aa_to_codons for the particular amino acid in current_AA
         for single_codon in aa_to_codons[current_AA]:
 
-            ####What's going on here####
+            # add the (next) codon in the list of codons for this aa
             new_dna_string = dna_string + single_codon
 
-            ####What's going on here####
-            check_combinations(new_dna_string,aa_string[1:])
+            # call the function on the aa_string minus the first amino acid in the sequence (recursive step)
+            checkCombinations(new_dna_string, aa_string[1:], melting_temp_threshold, output_file)
 
-# Main Script
+def createLogger(filename, logger_name, logging_conf=None):
+    """
+    create logger in filemode append and format name-levelname-message with package/module __name__ (best practice from logger tutorial)
+    :param filename: name of the file in which to log.
+    :param logger_name: __name__ is recommended as a best practice in logger.config eg you can call this like so: createLogger(<your_filename>, __name__)
+                     (__name__ is a special variable in python)
+    :param logging_conf: path to logging configuration file
+    :returns: an instance of the configured logger
+    """
+    # a config file is passed, load it
+    if logging_conf:
+        logging.config.fileConfig(logging_conf)  # should include at least what is below
+    # if it is not, configure as follows
+    else:
+        # create log for the year-month-day
+        logging.basicConfig(
+            filename='%s' % filename,
+            filemode='w',
+            format='%(name)s-%(levelname)s-%(asctime)s-%(message)s',
+            datefmt='%I:%M:%S %p',  # set 'datefmt' to hour-minute-second AM/PM
+            level='WARNING'
+        )
+    # return an instance of the configured logger
+    return logging.getLogger(logger_name)
 
-check_combinations( "", sys.argv[1] )
 
-
+if __name__ == "__main__":
+    main(sys.argv)
--- assignment13/README.txt
+++ assignment13/README.txt
@@ -1,15 +1,50 @@
 Usage:
 {How to run Polk.py}
--
+- Polk.py -p MGKGVLI WPWLEHM SIMTRED -m 58 60 # any number of sequences and temperatures can be entered. No commas, sequences all uppercase.
+                                                I used the sequences in the readme (which are different than the assignment prompt) for the README.
+                                                Feel free to replace them with the assignment prompt sequences.
+
+I inputted temperatures 58 and 60 to test the function of entering a list of items in both categories
+
 Output MGKGVLI:
 {Your oligos	calculated temps}
--
+
+- Amino Acid Sequence: MGKGVLI
+Melting Temperature: 58.00
+DNA Sequences:
+ATGGGCAAGGGCGTCCTCATC	58.26	ATGGGCAAGGGCGTCCTGATC	58.26	ATGGGCAAGGGCGTGCTCATC	58.26	ATGGGCAAGGGCGTGCTGATC	58.26	ATGGGCAAGGGGGTCCTCATC	58.26	ATGGGCAAGGGGGTCCTGATC	58.26	ATGGGCAAGGGGGTGCTCATC	58.26	ATGGGCAAGGGGGTGCTGATC	58.26	ATGGGGAAGGGCGTCCTCATC	58.26	ATGGGGAAGGGCGTCCTGATC	58.26	ATGGGGAAGGGCGTGCTCATC	58.26	ATGGGGAAGGGCGTGCTGATC	58.26	ATGGGGAAGGGGGTCCTCATC	58.26	ATGGGGAAGGGGGTCCTGATC	58.26	ATGGGGAAGGGGGTGCTCATC	58.26	ATGGGGAAGGGGGTGCTGATC	58.26
+
+Amino Acid Sequence: MGKGVLI
+Melting Temperature: 60.00
+DNA Sequences:
+
 Output WPWLEHM:
 {Your oligos	calculated temps}
--
+
+Amino Acid Sequence: WPWLEHM
+Melting Temperature: 58.00
+DNA Sequences:
+TGGCCATGGCTGGAGCACATG	58.26	TGGCCCTGGCTGGAACACATG	58.26	TGGCCCTGGCTTGAGCACATG	58.26	TGGCCCTGGTTGGAGCACATG	58.26	TGGCCGTGGCTGGAACACATG	58.26	TGGCCGTGGCTTGAGCACATG	58.26	TGGCCGTGGTTGGAGCACATG	58.26	TGGCCTTGGCTGGAGCACATG	58.26
+
+Amino Acid Sequence: WPWLEHM
+Melting Temperature: 60.00
+DNA Sequences:
+TGGCCCTGGCTGGAGCACATG	60.21	TGGCCGTGGCTGGAGCACATG	60.21
+
 Output SIMTRED:
 {Your oligos	calculated temps}
--
+
+Amino Acid Sequence: SIMTRED
+Melting Temperature: 58.00
+DNA Sequences:
+AGCATCATGACCCGCGAGGAC	58.26	AGCATCATGACCCGGGAGGAC	58.26	AGCATCATGACGCGCGAGGAC	58.26	AGCATCATGACGCGGGAGGAC	58.26	TCCATCATGACCCGCGAGGAC	58.26	TCCATCATGACCCGGGAGGAC	58.26	TCCATCATGACGCGCGAGGAC	58.26	TCCATCATGACGCGGGAGGAC	58.26
+
+Amino Acid Sequence: SIMTRED
+Melting Temperature: 60.00
+DNA Sequences:
+
 Comments:
 {Things that went wrong or you can not figure out}
--
+- In the assignment prompt sequences, there were none with restriction sites according to a cntrl - F search of the unfiltered output?
+  The sequences in the readme and the assignment prompt are different -- I used those specified in the readme for the readme. Feel free to enter
+  the assignment prompt sequences (all upper case, spaces between sequences)
